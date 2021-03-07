ba12618580032063656fb4e9cf44a2be5954c6da
Nadeeja Perera <nadeejaperera123@gmail.com>
2021-03-07 11:38:46 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import logo from "./logo.jpg";
+
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
+import UploadFiles from "./components/upload-files.component";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link to={"/tutorials"} className="nav-link">
+          <img src = {logo} alt="logo" />
+        </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/upload"} className="nav-link">
+                Upload File
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/tutorials/:id" component={Tutorial} />
+            <Route exact path="/upload" component={UploadFiles} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const path = __dirname + '/app/views/';
+
+const app = express();
+
+app.use(express.static(path));
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+global.__basedir = __dirname;
+
+const initRoutes = require("./app/routes");
+
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync();
+// drop the table if it already exists
+// db.sequelize.sync({
+//    force: true })
+// .then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+
+// simple route
+// app.get("/", (req, res) => {
+//   res.json({ message: "Welcome to bezkoder application." });
+// });
+
+require("./app/routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
